146afe6e9d605b11dc90270669bde2aec922dfb0
Duku Aghogho Mathida <mathildaduku@gmail.com>
2024-06-10 17:15:32 +0100

    implement create a new hotel functionality

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const hotelRouter = require('./routes/hotelRoutes');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/hotels', hotelRouter);
 
 module.exports = app;
---
+++ controllers/hotelController.js
@@ -0,0 +1,19 @@
+const Hotel = require('../models/hotelModel');
+
+exports.createHotel = async (req, res) => {
+  try {
+    const newHotel = await Hotel.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        hotel: newHotel,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
