7a15ab8655e142c58048af61b3cc25870597fec8
Duku Aghogho Mathida <104989791+mathildaduku@users.noreply.github.com>
2024-06-17 04:46:50 +0000

    implement global error handling middleware, add handler for undefined routes, add request time middleware

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const hotelRouter = require('./routes/hotelRoutes');
 
 const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString()
+})
+
+// Handler for defined routes
 app.use('/api/v1/hotels', hotelRouter);
+
+// Handler for undefined routes
+app.all('*', (req, res, next) => {
+ next(new AppError(`Can't find ${req.originalUrl} on this server`, 404))
+})
+
+// Registers Error handling middleware for entire application
+app.use(globalErrorHandler);
 
 module.exports = app;
