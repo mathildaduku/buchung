966eb65d7d120dd84901c66783dfb74b6c407e6d
Duku Aghogho Mathida <104989791+mathildaduku@users.noreply.github.com>
2024-06-17 06:38:27 +0000

    handle uncaught exceptions and unhandled promise rejecions for synchrnous code and asynchronous code respectively globally

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+// listen for uncaughtException events from synchronous code errors
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! SHUTTING DOWN.....')
+  console.log(err.name, err.message);
+  process.exit(1)
+})
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -15,6 +22,16 @@ mongoose.connect(DB, {}).then(() => console.log('DB connection successful!'));
 // start app server
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+// listen for unhandledRejection events from asybchronous code errors
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN...')
+
+  server.close(() => {
+    process.exit(1);
+  });
+})
