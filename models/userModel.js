1d817366c30b348358366e720e52241dfd682ebc
Duku Aghogho Mathida <104989791+mathildaduku@users.noreply.github.com>
2024-07-01 05:30:55 +0000

    fix indentation

--- models/userModel.js
+++ models/userModel.js
@@ -1,70 +1,76 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please provide your name!'],
+  name: {
+    type: String,
+    required: [true, 'Please provide your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email address.'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email address.'],
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 9,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
     },
-    email: {
-        type: String,
-        required: [true, 'Please provide your email address.'],
-        unique: true,
-        lowercase: true,
-        validate: [validator.isEmail, 'Please provide a valid email address.']
-    },
-    role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user',
-    },
-    password: {
-        type: String,
-        required: [true, 'Please provide a password'],
-        minlength: 9,
-        select: false,
-    },
-    passwordConfirm: {
-        type: String,
-        required: [true, 'Please confirm your password'],
-        validate: {
-            validator: function (el) {
-                return el === this.password;
-            },
-            message: 'Passwords are not the same!',
-        },
-    },
-    passwordChangedAt: Date,
+  },
+  passwordChangedAt: Date,
 });
 
 // use document middleware to enable encryption using bcrypt
 userSchema.pre('save', async function (next) {
-    // if the current document's password has not modified, exit the function and call the next middleware
-    if (!this.isModified('password')) return next();
+  // if the current document's password has not modified, exit the function and call the next middleware
+  if (!this.isModified('password')) return next();
 
-    this.password = await bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 12);
 
-    // remove passwordConfirm field from database
-    this.passwordConfirm = undefined;
-    next();
-})
+  // remove passwordConfirm field from database
+  this.passwordConfirm = undefined;
+  next();
+});
 
-// instance method to check if password is correct using bcrypt 
-userSchema.methods.correctPassword = async function ( candidatePassword, userPassword){
-    return await bcrypt.compare(candidatePassword, userPassword)
-} 
+// instance method to check if password is correct using bcrypt
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
-    if (this.passwordChangedAt) {
-        const changedTimeStamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+  if (this.passwordChangedAt) {
+    const changedTimeStamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10,
+    );
 
-        return JWTTimestamp < changedTimeStamp
-    }
+    return JWTTimestamp < changedTimeStamp;
+  }
 
-    return false;
-}
+  return false;
+};
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
