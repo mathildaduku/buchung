6eb9e49a20b8f04d1d46f58262f855abf4e816dd
Duku Aghogho Mathida <mathildaduku@gmail.com>
2024-07-12 18:55:36 +0100

    Enhance security and performance with various middleware

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp')
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,7 +15,40 @@ const bookingRouter = require('./routes/bookingRoutes');
 
 const app = express();
 
-app.use(express.json());
+// GLOBAL MIDDLEWARES
+
+// Use Helmet to set various HTTP headers to protect against common vulnerabilities
+app.use(helmet());
+
+// Sets rate limiting using rateLimit() which creates a middleware function we can use using app.use to prevent DDOS and brute force attacks by limiting the number of requests to the /api endpoint by an IP address
+const limiter = rateLimit({
+  max: 20,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+// Parse incoming JSON requests and limit the payload size to 10kb DDoS attacks and improve performance
+app.use(express.json({ limit: '10kb' }));
+
+// Santize user input to prevent MongoDB operator injection
+app.use(mongoSanitize());
+
+// Clean user input to protect against cross-site scripting (XSS) attacks
+app.use(xss());
+
+// Prevent http parameter pollution and whitelist specific parameters
+app.use(hpp({
+  whitelist: [
+    'city',
+    'starRating',
+    'room_type',
+    'availability',
+    'sort',
+    'fields',
+  ]
+}));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
